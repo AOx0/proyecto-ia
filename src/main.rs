8ae0dd923984d67a7121ce7a8b6a75e1caecf2ad
Alejandro Osornio <50227494+AOx0@users.noreply.github.com>
2023-05-31 06:51:02 +0000

    Optimization & clean

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,24 @@
-use std::fmt::{Debug, Display};
+use rand::{seq::IteratorRandom, Rng};
 
 fn main() {
-    let mut n8_queens = NQueen::<20>::default().into_random_state();
+    let mut n8_queens = NQueen::new(8).into_random_state();
 
+    let mut iterations = 0;
     while n8_queens.step() != 0 {
-        println!("{:?}", n8_queens);
+        // println!("{:?}\n", n8_queens);
+        iterations += 1;
     }
-    println!("{:?}", n8_queens);
+    println!("{}", iterations);
+    println!("{}", n8_queens);
 }
 
-struct NQueen<const N: usize> {
-    queens: [usize; N],
+struct NQueen {
+    n: usize,
+    queens: Vec<usize>,
+
+    last_queens: Vec<Vec<usize>>,
+    costs: Vec<(usize, usize, usize)>,
+    verbose: bool,
 }
 
 enum Side {
@@ -18,7 +26,7 @@ enum Side {
     Right,
 }
 
-impl<const N: usize> NQueen<N> {
+impl NQueen {
     fn cost_of(&self, of: usize) -> [usize; 3] {
         [
             self.column_c(of),
@@ -28,7 +36,7 @@ impl<const N: usize> NQueen<N> {
     }
 
     fn column_c(&self, of: usize) -> usize {
-        (0..N)
+        (0..self.n)
             .filter(|&x| x != of && self.queens[x] == self.queens[of])
             .count()
     }
@@ -36,7 +44,7 @@ impl<const N: usize> NQueen<N> {
     fn diagonal_c(&self, of: usize, side: Side) -> usize {
         let val = self.queens[of];
 
-        (0..N)
+        (0..self.n)
             .filter(|&x| {
                 if x != of {
                     let offset = x.checked_sub(of).unwrap_or_else(|| of - x);
@@ -52,106 +60,103 @@ impl<const N: usize> NQueen<N> {
             .count()
     }
 
-    fn into_random_state(self) -> Self {
-        let mut res = Self::default();
-        res.queens.iter_mut().for_each(|queen| {
-            *queen = rand::random::<usize>() % N;
+    fn into_random_state(mut self) -> Self {
+        self.last_queens.clear();
+        self.queens.iter_mut().for_each(|queen| {
+            *queen = rand::random::<usize>() % self.n;
         });
-        res
+        self
     }
 
     fn overall_cost(&self) -> usize {
-        (0..N)
+        (0..self.n)
             .map(|queen| self.cost_of(queen).into_iter().sum::<usize>())
             .sum::<usize>()
     }
 
     fn step(&mut self) -> usize {
-        let mut costs = (0..N)
-            .map(|queen| {
-                (
-                    queen,
-                    self.cost_of(queen).into_iter().sum::<usize>(),
-                    self.queens[queen],
-                )
-            })
-            .collect::<Vec<_>>();
+        let mut rng = rand::thread_rng();
 
-        costs.sort_by(|a, b| a.1.cmp(&b.1));
+        (0..self.n).for_each(|queen| {
+            self.costs[queen] = (
+                queen,
+                self.cost_of(queen).into_iter().sum::<usize>(),
+                self.queens[queen],
+            );
+        });
+        self.costs.sort_by(|a, b| a.1.cmp(&b.1));
 
-        let most_exp = costs.last().copied().unwrap();
-        costs = costs
-            .into_iter()
-            .filter(|&x| x.1 == most_exp.1)
-            .collect::<Vec<_>>();
-        let most_exp_idx = rand::random::<usize>() % costs.len();
-        let most_exp: (usize, usize, usize) = costs[most_exp_idx];
+        let worst_value = self.costs.last().and_then(|&x| Some(x.1)).unwrap();
+        let (worst_pos, _, prev_val) = self
+            .costs
+            .iter()
+            .filter(|&x| x.1 == worst_value)
+            .choose(&mut rng)
+            .copied()
+            .unwrap();
 
-        let mut new_costs = (0..N)
-            .map(|col| {
-                (col, {
-                    self.queens[most_exp.0] = col;
-                    self.cost_of(most_exp.0).iter().sum::<usize>()
-                })
-            })
-            .filter(|&x| x.0 != most_exp.2)
-            .collect::<Vec<_>>();
-        new_costs.sort_by(|a, b| a.1.cmp(&b.1));
+        (0..self.n).filter(|&col| col != prev_val).for_each(|col| {
+            self.costs[col] = (
+                col,
+                {
+                    self.queens[worst_pos] = col;
+                    let res = self.cost_of(worst_pos).iter().sum::<usize>();
+                    self.queens[worst_pos] = prev_val;
+                    res
+                },
+                0,
+            )
+        });
+        self.costs.sort_by(|a, b| a.1.cmp(&b.1));
 
-        let best_cost = new_costs.first().copied().unwrap();
-        new_costs = new_costs
-            .into_iter()
-            .filter(|&x| x.1 == best_cost.1)
-            .collect::<Vec<_>>();
-        let new_cost_idx = rand::random::<usize>() % new_costs.len();
-        let new_cost = new_costs[new_cost_idx];
+        let (_, best_cost, _) = self.costs[0];
+        let (new_cost, _, _) = self
+            .costs
+            .iter()
+            .filter(|&x| x.1 == best_cost)
+            .choose(&mut rng)
+            .copied()
+            .unwrap();
 
-        // println!("{:?}", costs);
-        // println!("{:?}", most_exp);
-        // println!("{:?}", new_costs);
-        // println!("{:?}", self.overall_cost());
-        self.queens[most_exp.0] = new_cost.0;
-        // println!("{:?}", self.overall_cost());
+        // Si hay colisión se trata de un camino sin salida. Forzamos algo de aleatoriedad
+        if self.last_queens.contains(&self.queens) {
+            self.queens[rng.gen_range(0..self.n)] = rng.gen_range(0..self.n);
+        } else {
+            self.last_queens.push(self.queens.clone())
+        }
+
+        self.queens[worst_pos] = new_cost;
         self.overall_cost()
     }
-}
 
-impl<const N: usize> Default for NQueen<N> {
-    fn default() -> Self {
-        NQueen { queens: [0; N] }
+    fn new(with_n: usize) -> Self {
+        NQueen {
+            n: with_n,
+            queens: vec![0; with_n],
+            last_queens: Vec::with_capacity(100),
+            costs: vec![(0, 0, 0); with_n],
+            verbose: false,
+        }
     }
 }
 
-impl<const N: usize> Display for NQueen<N> {
+impl std::fmt::Display for NQueen {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        (0..N).try_for_each(|row| {
-            (0..N)
+        (0..self.n).try_for_each(|row| {
+            (0..self.n)
                 .map(|i| if self.queens[row] == i { '*' } else { '.' })
                 .try_for_each(|val| write!(f, "{} ", val))?;
-            if row != N - 1 {
-                write!(f, "\n")?;
+
+            if self.verbose {
+                let cost = self.cost_of(row);
+                let [lval, rval, cval] = { [cost[0], cost[1], cost[2]] };
+                write!(
+                    f,
+                    " | ld:{lval:>2} rd:{rval:>2} cc:{cval:>2} | tt:{:>2}",
+                    cost.iter().sum::<usize>()
+                )?;
             }
-            Ok(())
-        })?;
-        Ok(())
-    }
-}
-
-impl<const N: usize> Debug for NQueen<N> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        (0..N).try_for_each(|row| {
-            (0..N)
-                .map(|i| if self.queens[row] == i { '*' } else { '.' })
-                .try_for_each(|val| write!(f, "{} ", val))?;
-
-            let cost = self.cost_of(row);
-            let [lval, rval, cval] = { [cost[0], cost[1], cost[2]] };
-            write!(
-                f,
-                " | ld:{lval:>2} rd:{rval:>2} cc:{cval:>2} | tt:{:>2}",
-                cost.iter().sum::<usize>()
-            )?;
-            if row != N - 1 {
+            if row != self.n - 1 {
                 write!(f, "\n")?;
             }
             Ok(())
